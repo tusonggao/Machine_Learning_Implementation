757c96e9a0bc4953b968b8da40c3718f424c22f6
涂松高 <tusonggao@163.com>
2018-08-28 02:20:46 +0800

    change file

--- k_nearest_neighbors.py
+++ k_nearest_neighbors.py
@@ -1,45 +1,108 @@
 from __future__ import print_function, division
-import numpy as np
-from mlfromscratch.utils import euclidean_distance
 
+import time
+import numpy as np
+import pandas as pd
+
+from collections import Counter
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_mldata
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+SEED = 911
+
+#mnist = fetch_mldata('MNIST original')
+#train_X, test_X, train_y, test_y = train_test_split(mnist.data,
+#            mnist.target, test_size=0.2, random_state=SEED)
+#print('after train_test_split, train_X.shape: ', train_X.shape,
+#      'train_y.shape: ', train_y.shape)
+
+
+train_df = pd.read_csv('C:/D_Disk/tsg_prog/Digit_Recognizer/mnist_train.csv')
+train_df = train_df.sample(5000)
+print('train_df.shape is ', train_df.shape)
+
+train_y = train_df['label']
+train_X = train_df.drop(['label'], axis=1)
+
+train_X, test_X, train_y, test_y = train_test_split(train_X,
+            train_y, test_size=0.1, random_state=SEED)
+
+test_y = test_y.values
+train_y = train_y.values
+
+print('after train_test_split, train_X.shape: ', train_X.shape,
+      'train_y.shape: ', train_y.shape)
 
 class KNN():
-    """ K Nearest Neighbors classifier.
-
-    Parameters:
-    -----------
-    k: int
-        The number of closest neighbors that will determine the class of the 
-        sample that we wish to predict.
-    """
     def __init__(self, k=5):
-        self.k = k
+        self.K = k
+        return
 
-    def _vote(self, neighbors):
-        """ Return the most common class among the neighbor samples """
-        counts = np.bincount(neighbors[:, 1].astype('int'))
-        return counts.argmax()
+    def fit(self, train_X, train_y):
+        if isinstance(train_X, pd.DataFrame) or isinstance(train_X, pd.Series):
+            train_X = train_X.values
+        if isinstance(train_y, pd.DataFrame) or isinstance(train_y, pd.Series):
+            train_y = train_y.values
 
-    def predict(self, X_test, X_train, y_train):
-        y_pred = np.empty(X_test.shape[0])
-        # Determine the class of each sample
-        for i, test_sample in enumerate(X_test):
-            # Two columns [distance, label], for each observed sample
-            neighbors = np.empty((X_train.shape[0], 2))
-            # Calculate the distance from each observed sample to the
-            # sample we wish to predict
-            for j, observed_sample in enumerate(X_train):
-                distance = euclidean_distance(test_sample, observed_sample)
-                label = y_train[j]
-                # Add neighbor information
-                neighbors[j] = [distance, label]
-            # Sort the list of observed samples from lowest to highest distance
-            # and select the k first
-            k_nearest_neighbors = neighbors[neighbors[:, 0].argsort()][:self.k]
-            # Get the most common class among the neighbors
-            label = self._vote(k_nearest_neighbors)
-            y_pred[i] = label
-        return y_pred
+        assert len(train_X)==len(train_y), 'train_X must has the same length as train_y'
+        self.train_X = train_X
+        self.train_y = train_y
+        return
     
-    ####################3
+    def get_result(self, x_vector):
+        distance_X = self.train_X - x_vector
+        distance_X **= 2
+        distance_arr = distance_X.sum(axis=1)
+        sorted_idx = np.argsort(np.sqrt(distance_arr))
+        y_labels = train_y[sorted_idx]
+        y_labels = Counter(y_labels[:self.K])
+        y = max(y_labels.keys(), key=lambda x:y_labels[x])
+        return y
+        
+    def predict(self, test_X):
+        if isinstance(test_X, pd.DataFrame):
+            test_X = test_X.values
+        
+        if len(test_X.shape)==1:
+            test_X.resize((1, len(test_X)))
+        
+        assert test_X.shape[1]==self.train_X.shape[1], 'test_X must has the same width as train_X'
+            
+        y_list = []
+        for i in range(test_X.shape[0]):
+            x_vector = test_X[i, :]
+            y = self.get_result(x_vector)
+            y_list.append(y)
+        
+        return np.array(y_list)
+    
+if __name__=='__main__':
+#    train_X = np.array([[1, 1, 1],
+#                        [2, 2, 2],
+#                        [3, 3, 3],
+#                        [4, 4, 4],
+#                        [5, 5, 5]])
+#    train_y = np.array([0, 1, 1, 1, 1])
+#    test_X = np.array([0, 0, 0])
+    
+    start_t = time.time()
+    knn = KNN(5)
+    knn.fit(train_X, train_y)
+    y_pred = knn.predict(test_X)
+#    print(test_X)
+#    print('y_pred is ', y_pred)
+    print('accuacy is ', accuracy_score(test_y, y_pred),
+          'cost time: ', time.time()-start_t)
+
+    start_t = time.time()
+    neigh = KNeighborsClassifier(n_neighbors=5)
+    neigh.fit(train_X, train_y) 
+    y_pred_sklearn = neigh.predict(test_X)
+    print('accuacy is ', accuracy_score(test_y, y_pred_sklearn),
+          'cost time: ', time.time()-start_t)
+    
+    
         
