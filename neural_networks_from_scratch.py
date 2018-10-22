b492fdc7acb8f2bc0305b0d80cb8d08ca8fec368
涂松高 <tusonggao@163.com>
2018-10-22 21:51:38 +0800

    modify neural_networks_from_scratch.py

--- neural_networks_from_scratch.py
+++ neural_networks_from_scratch.py
@@ -82,6 +82,52 @@ def get_accuracy_value(Y_hat, Y):
     Y_hat_ = convert_prob_into_class(Y_hat)
     return (Y_hat_==Y).all(axis=0).mean()
 
+def single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, 
+                                      A_prev, activation='relu'):
+    m = A_prev.shape[1]
+    
+    if activation is 'relu':
+        backward_activation_func = relu_backward
+    elif activation is 'sigmoid':
+        backward_activation_func = sigmoid_backward
+    else:
+        raise Exception('Non-supported activation function')
+
+    dz_curr = backward_activation_func(dA_curr, Z_curr)
+    dW_curr = np.dot(dz_curr, A_prev.T) / m
+    db_curr = np.sum(dz_curr, axis=1, keepdims=True)/m
+    dA_prev = np.dot(W_curr.T, dz_curr)
+
+    return dA_prev, dW_curr, db_curr
+
+def full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
+    grads_values = {}
+    m = Y.shape[1]
+    Y = Y.reshape(Y_hat.shape)
+
+    dA_prev = -(np.divide(Y, Y_hat) - np.divide(1-Y, Y_hat))
+
+    for layer_idx_prev, layer in reversed(list(enumerate(nn_architecture))):
+        layer_idx_curr = layer_idx_prev + 1
+        activ_function_curr = layer['activation']
+
+        dA_curr = dA_prev
+
+        A_prev = memory['A' + str(layer_idx_prev)]
+        Z_curr = memory['Z' + str(layer_idx_curr)]
+        W_curr = params_values['W' + str(layer_idx_curr)]
+        b_curr = params_values['b' + str(layer_idx_curr)]
+
+        dA_prev, dW_curr, db_curr = single_layer_backward_propagation(
+            dA_curr, W_curr, b_curr, Z_curr, A_prev, activ_function_curr
+        )
+
+        grads_values['dW' + str(layer_idx_curr)] = dW_curr
+        grads_values['db' + str(layer_idx_curr)] = db_curr
+
+    return grads_values
+
+
 
 
 
