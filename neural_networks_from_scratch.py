1a3e7e778ddd159af38f6cb7c89abdb09ff20259
涂松高 <tusonggao@163.com>
2018-10-22 21:16:22 +0800

    modify neural_networks_from_scratch.py

--- neural_networks_from_scratch.py
+++ neural_networks_from_scratch.py
@@ -39,6 +39,19 @@ def relu_backward(dA, Z):
     dZ[Z<=0] = 0
     return dZ
 
+def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation='relu'):
+    Z_curr = np.dot(W_curr, A_prev) + b_curr
+    if activation is 'relu':
+        activation_func = relu
+    elif activation is 'sigmoid':
+        activation_func = sigmoid
+    else:
+        raise Exception('Non-supported activation function')
+    
+    return activation_func(Z_curr), Z_curr
+
+
+
 
 
 
