0769351cf5b1ccdc168d991dd4eb2cf517c056a3
涂松高 <tusonggao@163.com>
2018-10-22 22:35:55 +0800

    modify test_py.py

--- test_py.py
+++ test_py.py
@@ -1,19 +1,58 @@
-import signal
-import resource
-import os
+import numpy as np
 
-def time_exceeded(signo, frame):
-    print("Time's up!")
-    raise SystemExit(1)
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-def set_max_runtime(seconds):
-    # Install the signal handler and set a resource limit
-    soft, hard = resource.getrlimit(resource.RLIMIT_CPU)
-    resource.setrlimit(resource.RLIMIT_CPU, (seconds, hard))
-    signal.signal(signal.SIGXCPU, time_exceeded)
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-if __name__ == '__main__':
-    set_max_runtime(15)
+from keras.models import Sequential
+from keras.layers import Dense
 
-    while True:
-        pass
+##############################################################################
+
+model = Sequential()
+model.add(Dense(25, input_dim=2, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(25, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+history = model.fit(X_train, y_train, epochs=200, verbose=0)
+Y_test_hat = model.predict_classes(X_test)
+acc_test = accuracy_score(y_test, Y_test_hat)
+print('Test set accuracy: {.2f} -Goliath'.format(acc_test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
